278f289b4edf340f57bbac0afe306580286fc453
geekou.io <112389125+geekouofficial@users.noreply.github.com>
2022-08-29 13:18:21 -0300

    Add files via upload

---
+++ geekou-githubgeeks/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Welcome to Geekou - Github Geeks v0</title>
+   <meta name="description" content="Search the web for sites and images.">
+   <meta name="keywords" content="Geekou - Github Geeks - Search Engine v0">
+   <meta name="author" content="Github Geeks v0">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+   <div class="wrapper indexPage">
+     <div class="mainSection">
+	<div class="logoContainer">
+	   <h1>Geekou Github Geeks</h1>
+	</div>
+	<div class="searchContainer">
+	   <form action="search.php" method="GET">
+	      <input class="searchBox" type="text" name="term" autocomplete="off">
+	      <input class="searchButton" type="submit" value="Search">
+	   </form>
+	</div>
+     </div>
+   </div>
+</body>
+</html>
